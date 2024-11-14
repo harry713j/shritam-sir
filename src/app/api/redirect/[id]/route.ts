992d674ee6cac41e785d5f5120b3fe838bd6f946
Harry <hariharasethi89@gmail.com>
2024-11-14 18:57:52 +0530

    Fix dynamic route error

--- src/app/api/redirect/[id]/route.ts
+++ src/app/api/redirect/[id]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ): Promise<NextResponse<ApiResponse>> {
   const { id } = await params;
   await connectDb();
